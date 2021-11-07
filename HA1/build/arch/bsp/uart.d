6b36a1f784d7e363a5841e1517693306ed3f26df
Ammar594 <65511043+Ammar594@users.noreply.github.com>
2021-11-07 14:24:23 +0100

    Delete HA1 directory

--- HA1/build/arch/bsp/uart.d
+++
@@ -1,2 +0,0 @@
-build/arch/bsp/uart.o: arch/bsp/uart.c include/arch/bsp/bcm2836.h
-include/arch/bsp/bcm2836.h:
--- HA1/build/kernel/start.d
+++
@@ -1,5 +0,0 @@
-build/kernel/start.o: kernel/start.c include/arch/bsp/kprintf.h \
- include/arch/bsp/uart.h include/config.h
-include/arch/bsp/kprintf.h:
-include/arch/bsp/uart.h:
-include/config.h:
